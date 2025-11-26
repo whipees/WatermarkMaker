9f576183b0d3df2ae319536fdad64f883fdafb6d
sebwhi <sebikjanecek@gmail.com>
2025-11-26 19:48:40 +0100

    added starting script

---
+++ bin/start.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from src import main
+
+if __name__ == "__main__":
+    main.run_app()
