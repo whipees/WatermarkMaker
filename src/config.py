4f6e7afb4bf99e1c4d19cc46daedf90eb3ebfcca
sebwhi <sebikjanecek@gmail.com>
2025-11-26 18:32:35 +0100

    added basic conf file and init file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.getcwd()
+INPUT_FOLDER = os.path.join(BASE_DIR, "input_images")
+OUTPUT_FOLDER = os.path.join(BASE_DIR, "output_watermarked")
+PROCESSED_FOLDER = os.path.join(BASE_DIR, "processed_originals")
+ERROR_FOLDER = os.path.join(BASE_DIR, "error_files")
+
+
+WATERMARK_FILE = os.path.join(BASE_DIR, "watermark.png")
