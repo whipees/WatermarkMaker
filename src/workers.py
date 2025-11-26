d3fb9c55463762f7c303f4a976641d45f4b33a33
sebwhi <sebikjanecek@gmail.com>
2025-11-26 19:11:46 +0100

    added worker method

--- src/workers.py
+++ src/workers.py
@@ -2,6 +2,7 @@ import time
 import os
 import queue
 from src import config,logger,image_processing
+import shutil
 
 task_queue = queue.Queue()
 running = True
@@ -30,3 +31,24 @@ def producer():
             time.sleep(1)
         except Exception as e:
             logger.log_msg("PRODUCER", f"Error: {e}")
+
+def worker(worker_id):
+    name = f"Worker {worker_id}"
+    logger.logger(name, "Starting worker {worker_id}")
+
+    while running:
+        try:
+            filepath,filename = task_queue.get(timeout=1)
+            if os.path.exists(filepath):
+                try:
+                    image_processing.apply_watermark(filepath,config.WATERMARK_FILE,config.OUTPUT_FOLDER,filename)
+                    logger.logger(name, "WATERMARK PROCESSED")
+                    shutil.move(filepath,os.path.join(config.PROCESSED_FOLDER,filename))
+                except Exception as e:
+                    logger.logger(name, f"error: {e}")
+                    if os.path.exists(filepath):
+                        shutil.move(filepath, os.path.join(config.ERROR_FOLDER, filename))
+
+            task_queue.task_done()
+        except queue.Empty:
+            continue
