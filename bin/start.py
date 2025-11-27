cfd25a4e03a4393052b1ebf853d0493dad8ef6ea
sebwhi <sebikjanecek@gmail.com>
2025-11-27 18:00:05 +0100

    added comments

--- bin/start.py
+++ bin/start.py
@@ -3,6 +3,9 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
+"""
+ přidání cesty do seznamu, kde Python hledá moduly
+"""
 sys.path.append(parent_dir)
 
 from src import main
