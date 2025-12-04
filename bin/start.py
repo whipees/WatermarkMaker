349edeed2c5e0017ea4447008564e45571aa4b15
sebwhi <sebikjanecek@gmail.com>
2025-12-04 17:18:42 +0100

    removed comment

--- bin/start.py
+++ bin/start.py
@@ -3,9 +3,7 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-"""
- přidání cesty do seznamu, kde Python hledá moduly
-"""
+
 sys.path.append(parent_dir)
 
 from src import main
